082cb2c2388bce6234ecafee9ba13c48f44c52c6
Elias Bosco <elias@engrene.com.br>
2023-09-25 10:25:41 +0100

    Graceful shutdown implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { logger } = require('./helpers/logger')
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./src/swagger/swagger.yaml');
-const basicAuth = require('express-basic-auth');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./src/swagger/swagger.yaml')
+const basicAuth = require('express-basic-auth')
 
 let app
 const port = process.env.SERVICE_PORT || 3001
@@ -41,7 +41,7 @@ const init = async () => {
           password,
           process.env.SWAGGER_PASSWORD || 'deel'
       );
-      return userMatches && passwordMatches;
+      return userMatches && passwordMatches
     }
 
     app.use(
@@ -52,31 +52,44 @@ const init = async () => {
       }),
       swaggerUi.serve,
       swaggerUi.setup(swaggerDocument, swaggerUiOptions)
-    );
+    )
 
     app.use(bodyParser.json())
     app.use(cookieParser())
 
-    const { routes } = require('./routes');
+    const { routes } = require('./routes')
     routes(app)
 
-    if (!module.parent) {
-      app.listen(port, () => {
-        logger.info(`Listening on Port ${port}`)
-      })
-    }
+    const server = app.listen(port, () => {
+      logger.info(`Listening on Port ${port}`)
+    })
+
+    // Gracefully closing
+    process.on('SIGTERM', async () => {
+      await gracefulShutdown('SIGTERM', server)
+    })
+
+    process.on('SIGINT', async () => {
+      await gracefulShutdown('SIGINT', server)
+    })
   } catch (error) {
     logger.error(`An error occurred: ${JSON.stringify(error)}`)
-    process.exit(1);
+    process.exit(1)
   }
+}
+
+const gracefulShutdown = async (sigVal, server) => {
+  logger.info(`${sigVal} signal received.`);
+  logger.info('Closing http server.');
+  server.close(() => {
+    logger.info('Http server closed.');
+    // Close any other services here
+    // ...
+  })
 }
 
 (async () => {
   app = await init()
-
-  process.on('SIGTERM', async () => {
-    logger.info(`Service finished with SIGTERM`);
-  })
 })()
 
 module.exports = {
